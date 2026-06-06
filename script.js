8239f3e4b905cb3e68849ba0663368a85898cad5
Deepak <darkdeepak001@gmail.com>
2026-06-06 21:49:11 +0530

    Add initial form handling in script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+var n = document.getElementById("name")
+var a = document.getElementById("age")
+var g = document.getElementById("gender") 
+var c = document.getElementById("course")
+var e = document.getElementById("email")
+var d = document.getElementById("data")
+function save()
+{
+    
+    var d.innerHTML = document.createElement("table") 
+    
+}
