bee3ee61a48e922bcc2b330420ed64188ca4e87b
Deepak <darkdeepak001@gmail.com>
2026-06-07 05:39:12 +0530

    Rename 'data' element to 'task' and update save function

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ var a = document.getElementById("age")
 var g = document.getElementById("gender") 
 var c = document.getElementById("course")
 var e = document.getElementById("email")
-var d = document.getElementById("data")
+var d = document.getElementById("task")
 function save()
 {
     
-    var d.innerHTML = document.createElement("table") 
-    
+    var row = document.createElement("tr")
+    row.innerHTML = "<td>"+n.value+"</td>"+"<td>"+a.value+"</td>"+"<td>"+g.value+"</td>"+"<td>"+c.value+"</td>"+"<td>"+e.value+"</td>"+"<td><button onclick=deleteitem(event)>Delete</button></td>"
+    d.append(row)
+}
+function deleteitem(event)
+{
+    event.target.parentElement.parentElement.remove()
 }
