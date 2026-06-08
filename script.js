cd59577b2f6995a8f86cca7f888642e3bf96ac51
Deepak <darkdeepak001@gmail.com>
2026-06-08 06:51:31 +0530

    Add edititem function to handle item editing

--- script.js
+++ script.js
@@ -15,3 +15,13 @@ function deleteitem(event)
 {
     event.target.parentElement.parentElement.remove()
 }
+function edititem(event)
+{
+    var row = event.target.parentElement.parentElement
+    n.value = row.children[0].textContent
+    a.value = row.children[1].textContent
+    g.value = row.children[2].textContent
+    c.value = row.children[3].textContent
+    e.value = row.children[4].textContent
+    row.remove()
+}
